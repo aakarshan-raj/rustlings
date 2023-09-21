d7b8f4c059769976ff2d95297754e035a6c08e8f
aakarshan-raj <aakarshan7raj@gmail.com>
2023-09-22 00:02:24 +0530

    trait1

--- exercises/traits/traits1.rs
+++ exercises/traits/traits1.rs
@@ -7,20 +7,21 @@
 // Execute `rustlings hint traits1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 trait AppendBar {
-    fn append_bar(self) -> Self;
+    fn append_bar(&mut self) -> Self;
 }
 
 impl AppendBar for String {
-    fn append_bar(&mut self)->Self{
-        (self.push_str("Bar")).to_string()
+    fn append_bar(&mut self) -> Self {
+        self.push_str("Bar");
+        let x = self.clone();
+        x
     }
 }
 
 fn main() {
-    let s = String::from("Foo");
+    let mut s = String::from("Foo");
     let s = s.append_bar();
     println!("s: {}", s);
 }
